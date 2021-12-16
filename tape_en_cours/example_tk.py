be9525fefd21c1dfc643c08b3c9e2f376798f9b6
Matthieu Falce <falce.matthieu@gmail.com>
2021-12-16 16:25:50 +0100

    gitautosync update 174

--- tape_en_cours/example_tk.py
+++ tape_en_cours/example_tk.py
@@ -4,12 +4,14 @@ import time
 
 
 def traitement():
+    print("on lance")
     time.sleep(10)
+    print("c'est fini")
 
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Click me", command=root.destroy).grid(column=1, row=0)
+ttk.Button(frm, text="Launch compute", command=traitement).grid(column=1, row=0)
 root.mainloop()
