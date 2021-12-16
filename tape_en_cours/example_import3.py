1f090f42e85fb0a18f45a2ea014b3aeaed330747
Matthieu Falce <falce.matthieu@gmail.com>
2021-12-16 11:01:09 +0100

    gitautosync update 131

--- tape_en_cours/example_import3.py
+++ tape_en_cours/example_import3.py
@@ -1,5 +1,7 @@
+from a_importer import code_metier
+
+print(code_metier.a)
 from example_import2 import code_metier as code_metier2
 from example_import import code_metier as code_metier1
-from a_importer import code_metier
 
 print(code_metier is code_metier1 and code_metier1 is code_metier2)
